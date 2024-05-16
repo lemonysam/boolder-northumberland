2a934bd64870cf5d41fc7d19f68bd4a75d37f5b7
lemonysam <sam.cartwright@gmail.com>
2024-05-16 13:31:41 +0100

    Auto populate missing locations from topos if available (#2)

--- app/models/topo.rb
+++ app/models/topo.rb
@@ -1,8 +1,4 @@
 class Topo < ApplicationRecord
-  has_one_attached :photo do |attachable|
-    attachable.variant :medium, resize_to_fill: [1200, 900], saver: { quality: 50, strip: true, interlace: true }, preprocessed: true
-  end
-
   has_many :lines, dependent: :destroy
   has_many :problems, through: :lines
 
@@ -11,6 +7,17 @@ class Topo < ApplicationRecord
   scope :published, -> { where(published: true) }
 
   validates :photo, presence: true
+
+  after_commit -> do
+    photo.attachment.blob.open do |file|
+      gps_data = EXIFR::JPEG.new(file).gps
+      prefill_problems_with_gps_data gps_data if gps_data.present?
+    end
+  end
+
+  has_one_attached :photo do |attachable|
+    attachable.variant :medium, resize_to_fill: [1200, 900], saver: { quality: 50, strip: true, interlace: true }, preprocessed: true
+  end
 
   def metadata_latitude
     metadata["latitude"] 
@@ -26,5 +33,14 @@ class Topo < ApplicationRecord
 
   def metadata_heading
     metadata["heading"] 
+  end
+
+  private
+
+  def prefill_problems_with_gps_data(gps_data)
+    problems.without_location.each do |problem|
+      problem.location = FACTORY.point(gps_data.longitude, gps_data.latitude)
+      problem.save!
+    end
   end
 end
