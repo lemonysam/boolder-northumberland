bf39003de40df9535eab716b957b1bb4522a56d7
Sam Cartwright <sam.cartwright@gmail.com>
2024-06-04 21:00:35 +0100

    add CSV file import problem functionality

--- app/controllers/admin/problem_imports_controller.rb
+++ app/controllers/admin/problem_imports_controller.rb
@@ -1,19 +1,30 @@
 class Admin::ProblemImportsController < Admin::BaseController
   def create
-    files = params[:import][:json].select{|f| f.present? }
+    files = params[:import][:csv].select{|f| f.present? }
 
     files.sort_by{|file| file.original_filename }.each do |file|
-      hash = JSON.parse(file.read)
-      problem = Problem.find hash["problemId"]
+      array = CSV.parse(file.read, headers: true)
+      array.each do |hash|
+        problem = Problem.new(
+          area: Area.find(hash["areaId"].to_i),
+          name: hash["name"],
+          grade: hash["grade"],
+          steepness: hash["steepness"].present? ? hash["steepness"].to_sym : :other,
+          height: hash["height"],
+          sit_start: hash["sit_start"] == "true",
+          description_en: hash["description_en"],
+          history_note: hash["history_note"],
+        )
 
-      problem.height = hash["height"]
-      problem.steepness = hash["steepness"]
-      problem.landing = hash["landingDifficulty"]
-
-      problem.save!
+        puts problem.inspect
+        problem.save!
+      end
     end
 
     flash[:notice] = "#{files.count} problems updated"
+    redirect_to new_admin_problem_import_path
+  rescue => err
+    flash[:error] = err.message
     redirect_to new_admin_problem_import_path
   end
 end
