8c2667ec750a7ecf18e361c36c580d54f3a34622
lemonysam <sam.cartwright@gmail.com>
2025-01-08 15:10:29 +0000

    Debug worker build

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,8 +1,8 @@
+Sidekiq.configure_client do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
 AlgoliaSearch.configuration = { 
   application_id: Rails.application.credentials.dig(:algolia, :app_id),
   api_key: Rails.application.credentials.dig(:algolia, :api_key) 
 }
-
-Sidekiq.configure_client do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-end
