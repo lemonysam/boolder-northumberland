ee838af4fbdf1cf65568268f064af13e55f0b703
lemonysam <sam.cartwright@gmail.com>
2025-01-12 13:46:53 +0000

    lazy sidekiq initialisation debug

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,8 @@
+puts '****************************'
+puts '****************************'
+puts '****************************'
+puts '****************************'
+
 Sidekiq.configure_server do |config|
     config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
