d2743bbf51309654790389af9ae1e5206e80708f
lemonysam <sam.cartwright@gmail.com>
2025-01-12 14:17:28 +0000

    add cameron

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,3 @@
-puts '****************************'
-puts '****************************'
-puts '****************************'
-puts '****************************'
-
 Sidekiq.configure_server do |config|
     config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
