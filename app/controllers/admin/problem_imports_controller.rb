e647acaf3f9ecdc6c2a924d9d8ae8b51b9c9beb6
lemonysam <sam.cartwright@gmail.com>
2025-01-23 19:06:24 +0000

    add some messaging to imports

--- app/controllers/admin/problem_imports_controller.rb
+++ app/controllers/admin/problem_imports_controller.rb
@@ -1,7 +1,9 @@
 class Admin::ProblemImportsController < Admin::BaseController
   def create
     files = params[:import][:csv].select{|f| f.present? }
-
+    update_count = 0
+    create_count = 0
+    fail_count = 0
     files.sort_by{|file| file.original_filename }.each do |file|
       array = CSV.parse(file.read, headers: true)
       array.each do |hash|
@@ -19,6 +21,8 @@ class Admin::ProblemImportsController < Admin::BaseController
           problem.history_note = hash["history_note"]
           
           problem.save!
+
+          create_count += 1
         else
           problem = Problem.new(
             area: Area.find(hash["areaId"].to_i),
@@ -32,14 +36,18 @@ class Admin::ProblemImportsController < Admin::BaseController
             description_en: hash["description_en"],
             history_note: hash["history_note"],
           ).save!
+
+          update_count += 1
         end
+        rescue => err
+          fail_count += 1
+          flash[:error] = "#{err.message}"
       end
     end
 
-    flash[:notice] = "#{files.count} problems updated"
+    flash[:success] = "#{create_count} problems created"
+    flash[:notice] = "#{update_count} problems updated"
+    flash[:error] = "#{fail_count} problems failed #{flash[:error]}"
     redirect_to new_admin_problem_import_path
-  # rescue => err
-  #   flash[:error] = err.message
-  #   redirect_to new_admin_problem_import_path
   end
 end
